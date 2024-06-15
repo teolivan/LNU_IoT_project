a43edc3905564a96e70bfb6f9c9b8ecf368d8c25
olivia svensson <oliviateoliva@gmail.com>
2024-06-16 00:49:40 +0200

    Update main.py

--- climate/main.py
+++ climate/main.py
@@ -43,28 +43,3 @@ while True:
     except Exception as error:
         print("Exception occurred", error)
     time.sleep(1)
-
-
-
-
-adc = machine.ADC(27)
-sf = 4095/65535 # Scale factor
-volt_per_adc = (3.3 / 4095)
-
-while True: 
-    mv = adc.read_u16()
-
-    adc_12b = mv * sf
-
-    volt = adc_12b * volt_per_adc
-
-    # MCP9700 characteristics
-    dx = abs(50 - 0)
-    dy = abs(0 - 0.5)
-
-    shift = volt - 0.5
-
-    temp = shift / (dy / dx)
-    print(temp)
-    time.sleep(1)
-
