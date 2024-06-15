fe21c49cc295e98aca3445bea39aea770b47d6ee
olivia svensson <oliviateoliva@gmail.com>
2024-06-16 00:48:19 +0200

    add MCP9700 temperature functionality

--- climate/main.py
+++ climate/main.py
@@ -7,14 +7,24 @@ import time
 dht11 = dht.DHT11(machine.Pin(22))     # DHT11 Constructor 
 ldr = ADC(Pin(27))                     #LDR Constructor
 led = Pin("LED", Pin.OUT)              #LED Contstructor
+mcp9700 = machine.ADC(26)              #MCP9700 Constructor
+
+# MCP9700 characteristics
+V0C = 0.5  # Voltage output from MCP9700 at 0°C (0.5V)
+TCS = 0.01  # Temperature coefficient of MCP9700 (10mV/°C or 0.01V/°C)
+VREF = 3.3  # Reference voltage from the system (Raspberry Pi Pico W) (3.3V)
+MAX_ADC_VALUE = 65535  # Maximum value for a 16-bit ADC
 
 
 while True:
     try:
+        # DHT11
         dht11.measure()
        # temperature = dht11.temperature() <-- could measure this, but the MCP9700 sensor is more accurate than the DHT11 at measuring the temperature
         humidity = dht11.humidity()
         print("Humidity is " + str(humidity))
+
+        # LDR
         light = ldr.read_u16()
         darkness = round(light / 65535 * 100, 2)
         if darkness >= 70:
@@ -23,9 +33,38 @@ while True:
         else:
             print("It is enough light, no need to turn the LED on")
             led.off()
+        
+        #MCP9700
+        raw_adc_value = mcp9700.read_u16() # Read the raw ADC value (16-bit)
+        voltage = (raw_adc_value / MAX_ADC_VALUE) * VREF # Convert the raw ADC value to voltage
+        temperature = (voltage - V0C) / TCS # Temperature in Celsius
+        print("Temperature: {:.2f} °C".format(temperature))
+
     except Exception as error:
-        print("Exception occurred with humidity sensor DHT11", error)
+        print("Exception occurred", error)
     time.sleep(1)
 
 
+
+
+adc = machine.ADC(27)
+sf = 4095/65535 # Scale factor
+volt_per_adc = (3.3 / 4095)
+
+while True: 
+    mv = adc.read_u16()
+
+    adc_12b = mv * sf
+
+    volt = adc_12b * volt_per_adc
+
+    # MCP9700 characteristics
+    dx = abs(50 - 0)
+    dy = abs(0 - 0.5)
+
+    shift = volt - 0.5
+
+    temp = shift / (dy / dx)
+    print(temp)
+    time.sleep(1)
 
