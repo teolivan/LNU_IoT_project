dfb175f70b574c74ac0a75e0217c5f497c114a4d
olivia svensson <oliviateoliva@gmail.com>
2024-06-22 16:12:27 +0200

    add connectivity

--- climate/main.py
+++ climate/main.py
@@ -9,9 +9,9 @@ dht11 = dht.DHT11(machine.Pin(22))     # DHT11 Constructor
 ldr = ADC(Pin(27))                     # LDR Constructor
 led = Pin("LED", Pin.OUT)              # LED Contstructor
 mcp9700 = machine.ADC(26)              # MCP9700 Constructor
-#red = LED(19)                          # LED 5mm red diffuse 1500mcd Constructor
-#green = LED(20)                        # LED 5mm green diffuse 1500mcd Constructor
-#yellow = LED(21)                       # LED 5mm yellow diffuse 1500mcd Constructor
+red = machine.Pin(19, machine.Pin.OUT)                          # LED 5mm red diffuse 1500mcd Constructor
+green = machine.Pin(20, machine.Pin.OUT)                         # LED 5mm green diffuse 1500mcd Constructor
+yellow = machine.Pin(21, machine.Pin.OUT)                        # LED 5mm yellow diffuse 1500mcd Constructor
 
 
 # MCP9700 characteristics
@@ -23,12 +23,14 @@ MAX_ADC_VALUE = 65535  # Maximum value for a 16-bit ADC
 
 while True:
     try:
-      #  yellow.on()
-       # green.on()
-        #red.on()
+        yellow.toggle()
+        green.toggle()
+        
+        red.toggle()
         # DHT11
         dht11.measure()
        # temperature = dht11.temperature() <-- could measure this, but the MCP9700 sensor is more accurate than the DHT11 at measuring the temperature
+       # delta between dht11 and mcp9700 check value for temp, compare 
         humidity = dht11.humidity()
         print("Humidity is " + str(humidity))
 
