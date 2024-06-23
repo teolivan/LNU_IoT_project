e03ac58b650ba50037677bdba918dd852e235a75
olivia svensson <oliviateoliva@gmail.com>
2024-06-23 14:48:22 +0200

    Update main.py

--- climate/main.py
+++ climate/main.py
@@ -56,6 +56,22 @@ def humidityEvaluation(name, humidity, min_humidity, max_humidity):
         print("Humidity is " + str(humidity) + " which is very low for a/an " + name) 
 
     
+def lightEvaluation(name, darkness, min_darkness, max_darkness):
+    if darkness > min_darkness and darkness < max_darkness:
+        leds[0].toggle()
+        print("Darkness is " + str(darkness) + " which is good for a/an " + name)
+    elif darkness < min_darkness and darkness > (min_darkness * 0.9): 
+        leds[1].toggle()
+        print("Darkness is " + str(darkness) + " which is not enough for a/an " + name)
+    elif darkness < (min_darkness * 0.9): 
+        leds[2].toggle()
+        print("Darkness is " + str(darkness) + " which is way too little for a/an " + name)
+    elif darkness > max_darkness and darkness < (max_darkness * 1.1):
+        leds[1].toggle()
+        print("Darkness is " + str(darkness) + " which is a little much for a/an " + name)
+    elif darkness > (max_darkness * 1.1): 
+        leds[2].toggle()
+        print("Darkness is " + str(darkness) + " which is way too much for a/an " + name) 
 
 
 
@@ -65,6 +81,8 @@ if choice == "1":
     min_humidity = 55 # https://www.justaddiceorchids.com/orchid-care-blog/just-add-ice-orchid-blog/bid/98302/how-does-humidity-affect-my-orchid
     max_humidity = 75 # https://www.justaddiceorchids.com/orchid-care-blog/just-add-ice-orchid-blog/bid/98302/how-does-humidity-affect-my-orchid
     name = "orchid"
+    min_darkness = 40 # https://planterhoma.com/blogs/orchid-care/orchid-light-requirement
+    max_darkness = 50 # https://planterhoma.com/blogs/orchid-care/orchid-light-requirement
     while True:
         try:
             # DHT11
@@ -77,12 +95,7 @@ if choice == "1":
             # LDR
             light = ldr.read_u16()
             darkness = round(light / 65535 * 100, 2)
-            if darkness >= 70:
-                print("Darkness is {}%, LED turned on".format(darkness))
-                led.on()
-            else:
-                print("It is enough light, no need to turn the LED on")
-                led.off()
+            lightEvaluation(name, darkness, min_darkness, max_darkness)
             
             #MCP9700
             raw_adc_value = mcp9700.read_u16() # Read the raw ADC value (16-bit)
@@ -99,6 +112,8 @@ elif choice == 2:
     min_humidity = 70 # https://mossclerks.co.uk/pages/moss-caring-guide
     max_humidity = 80 # https://mossclerks.co.uk/pages/moss-caring-guide
     name = "moss"
+    min_darkness = 25 # https://greg.app/hypnum-moss-light-requirements/
+    max_darkness = 45 # https://greg.app/hypnum-moss-light-requirements/
     while True:
         try:
             # DHT11
@@ -108,7 +123,11 @@ elif choice == 2:
             humidity = dht11.humidity()
             humidityEvaluation(name, humidity, min_humidity, max_humidity)
 
-                
+             # LDR
+            light = ldr.read_u16()
+            darkness = round(light / 65535 * 100, 2)
+            lightEvaluation(name, darkness, min_darkness, max_darkness)
+                 
             #MCP9700
             raw_adc_value = mcp9700.read_u16() # Read the raw ADC value (16-bit)
             voltage = (raw_adc_value / MAX_ADC_VALUE) * VREF # Convert the raw ADC value to voltage
@@ -124,6 +143,8 @@ elif choice == 3:
     min_humidity = 40 # https://www.justaddiceorchids.com/orchid-care-blog/just-add-ice-orchid-blog/bid/98302/how-does-humidity-affect-my-orchid
     max_humidity = 60 # https://www.justaddiceorchids.com/orchid-care-blog/just-add-ice-orchid-blog/bid/98302/how-does-humidity-affect-my-orchid
     name = "cactus"
+    min_darkness = 0 # https://www.gardenhealth.com/advice/plants-flowers/how-to-care-for-cacti-and-succulents
+    max_darkness = 30 # https://www.gardenhealth.com/advice/plants-flowers/how-to-care-for-cacti-and-succulents
     while True:
         try:
             # DHT11
@@ -133,7 +154,11 @@ elif choice == 3:
             humidity = dht11.humidity()
             humidityEvaluation(name, humidity, min_humidity, max_humidity)
 
-               
+            # LDR
+            light = ldr.read_u16()
+            darkness = round(light / 65535 * 100, 2)
+            lightEvaluation(name, darkness, min_darkness, max_darkness)
+            
             #MCP9700
             raw_adc_value = mcp9700.read_u16() # Read the raw ADC value (16-bit)
             voltage = (raw_adc_value / MAX_ADC_VALUE) * VREF # Convert the raw ADC value to voltage
